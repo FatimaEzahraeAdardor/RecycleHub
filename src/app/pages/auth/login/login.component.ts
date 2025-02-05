85439aac4107d10aa71b8e53909f2c0e4fa9aec6
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2025-02-05 10:15:43 +0100

    adding login UI

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {NgStyle} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    NgStyle,
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
