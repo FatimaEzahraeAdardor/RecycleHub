895ba56cdfb8be2949cf3a2c5d0ad62f069f3df3
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2025-02-08 08:12:57 +0100

    delete collection for particular done

--- src/app/core/service/collection.service.ts
+++ src/app/core/service/collection.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Collection } from "../../model/collection";
 import { Observable } from "rxjs";
+import {User} from "../../model/user";
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,8 @@ export class CollectionService {
 
   getCollectionsByParticular(particularId: string): Observable<Collection[]> {
     return this.http.get<Collection[]>(`${this.apiUrl}?particularId=${particularId}`);
+  }
+  deleteCollection(collectionId: string | null): Observable<Collection> {
+    return this.http.delete<Collection>(`${this.apiUrl}/${collectionId}`);
   }
 }
--- src/app/pages/particular/collections/collection/collection.component.ts
+++ src/app/pages/particular/collections/collection/collection.component.ts
@@ -27,14 +27,38 @@ export class CollectionComponent implements OnInit {
     if (this.particularId) {
       this.getCollections();
     } else {
-      console.warn('Aucun ID particulier trouvé.');
+      console.warn('No particular ID found.');
     }
   }
 
   getCollections() {
     this.collectionService.getCollectionsByParticular(this.particularId!).subscribe({
       next: (data) => this.collections = data,
-      error: (err) => console.error('Erreur lors de la récupération des collectes :', err)
+      error: (err) => console.error('Error while fetching collections:', err)
     });
+  }
+
+  deleteCollection(collectionId: string, status: string): void {
+    if (status !== 'pending') {
+      alert('You can only delete collections with "pending" status.');
+      return;
+    }
+
+    const confirmDelete = window.confirm('Are you sure you want to delete this collection?');
+    if (confirmDelete) {
+      this.collectionService.deleteCollection(collectionId).subscribe({
+        next: (response) => {
+          this.collections = this.collections.filter(collection => collection.id !== collectionId);
+          console.log('Collection deleted successfully:', response);
+        },
+        error: (error) => {
+          console.error('Error while deleting the collection:', error);
+        }
+      });
+    }
+  }
+
+  updateCollection(collectionId: string): void {
+    console.log('Update collection with ID:', collectionId);
   }
 }
